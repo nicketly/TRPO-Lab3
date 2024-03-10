814df2f56317ad58805d27ac6983c8b24d66b8f7
nicketly <dleinetu2016@gmail.com>
2024-03-10 17:41:30 +0500

    Commit 3.2

--- TRPO-Lab3/TRPO-Lab3.Lib/FormulaLib.cs
+++ TRPO-Lab3/TRPO-Lab3.Lib/FormulaLib.cs
@@ -1,7 +1,10 @@
 ﻿namespace TRPO_Lab3.Lib
 {
-    public class Class1
+    public class FormulaLib
     {
-
+        public static double Pyramid_Reg4_Volume(double a, double h)  //фигура_вид_величина. Здесь: пирамида_правильная четырехугольная_объем
+        {
+            return h / 3 * Math.Pow(a, 2);
+        }
     }
 }
