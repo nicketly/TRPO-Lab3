3913a0b8e9c897026c5fc75f52cd4984fa343472
nicketly <dleinetu2016@gmail.com>
2024-03-10 19:40:49 +0500

    Commit 3.4

--- TRPO-Lab3/TRPO-Lab3.ConsoleApp/Program.cs
+++ TRPO-Lab3/TRPO-Lab3.ConsoleApp/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TRPO_Lab3.Lib;
+
+double a = 0;
+double h = 0;
+
+void print(string str)
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.Write(str);
+    Console.ForegroundColor = ConsoleColor.White;
+};
+
+double take()
+{
+    double param = Convert.ToDouble(Console.ReadLine());
+    return param;
+}
+
+print("- The volume of a regular quadrangular pyramid - \n\n");
+
+print("Enter the length: ");
+a = take();
+
+print("Enter the height: ");
+h = take();
+
+print("\n");
+
+double result = Math.Round(FormulaLib.Pyramid_Reg4_Volume(a, h), 3);
+print("Result: ");
+print(Convert.ToString(result));
+print("\n");
