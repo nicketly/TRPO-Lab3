b76bc259357dedbdc9850d45e1535d25aec450f9
nicketly <dleinetu2016@gmail.com>
2024-03-22 19:04:14 +0500

    Commit 4.1

---
+++ TRPO-Lab3/TRPO-Lab3.WPFA/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TRPO_Lab3.Lib;
+
+namespace TRPO_Lab3.WPFA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (Height.Text == "")
+            {
+                Height.Text = "Height";
+            }
+
+            if (Length.Text == "")
+            {
+                Length.Text = "Length";
+            }
+
+            if (Height.Text != "" && Length.Text != "")
+            {
+                var vm = new Params();
+                DataContext = vm;
+            }
+            
+        }
+
+    }
+}
---
+++ TRPO-Lab3/TRPO-Lab3.WPFA/Params.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRPO_Lab3.Lib;
+
+namespace TRPO_Lab3.WPFA
+{
+    public class Params
+    {
+        public double Length { set; get; }
+        public double Height { set; get; }
+
+        public double Volume;
+        public double Result
+        {
+            get { return FormulaLib.Pyramid_Reg4_Volume(this.Length, this.Height); }
+            set { Volume = value; }
+        }
+    }
+}
