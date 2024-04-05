bf9bb7fe00f7f73cb066479141b54cac898ed06e
nicketly <dleinetu2016@gmail.com>
2024-04-05 21:03:17 +0500

    Commit 5.2

---
+++ TRPO-Lab3/TRPO-Lab3.WebApp/Controllers/CustomController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TRPO_Lab3.Lib;
+using TRPO_Lab3.WebApp.Models;
+
+namespace TRPO_Lab3.WebApp.Controllers
+{
+    public class CustomController : Controller
+    {
+        public ActionResult Custom_View(double length, double height)
+        {
+            var volume = FormulaLib.Pyramid_Reg4_Volume(length, height);
+            var vm = new CustomModel(length, height, volume);
+            return View(vm);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Custom_View(IFormCollection collection, double length, double height)
+        {
+            var volume = FormulaLib.Pyramid_Reg4_Volume(length, height);
+            var vm = new CustomModel(length, height, volume);
+            return View(vm);
+        }
+    }
+}
--- TRPO-Lab3/TRPO-Lab3.WebApp/Models/CustomModel.cs
+++ TRPO-Lab3/TRPO-Lab3.WebApp/Models/CustomModel.cs
@@ -1,11 +1,11 @@
 ﻿namespace TRPO_Lab3.WebApp.Models
 {
-    public class Model
+    public class CustomModel
     {
         public double Length { get; set; }
         public double Height { get; set; }
         public double Volume { get; set; }
-        public Model(double length, double height, double volume)
+        public CustomModel(double length, double height, double volume)
         {
             Length = length;
             Height = height;
