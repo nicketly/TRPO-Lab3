e6a1fed9e271748320ef4f66e1f853d9fcea4cca
nicketly <dleinetu2016@gmail.com>
2024-04-05 20:45:24 +0500

    Commit 5.1

---
+++ TRPO-Lab3/TRPO-Lab3.WebApp/Models/Model.cs
@@ -0,0 +1,15 @@
+﻿namespace TRPO_Lab3.WebApp.Models
+{
+    public class Model
+    {
+        public double Length { get; set; }
+        public double Height { get; set; }
+        public double Volume { get; set; }
+        public Model(double length, double height, double volume)
+        {
+            Length = length;
+            Height = height;
+            Volume = volume;
+        }
+    }
+}
